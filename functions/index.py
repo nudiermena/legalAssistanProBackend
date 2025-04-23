be6c159208f47ff262022461fe21f0bd2ebb5985
Nudier Mena <nudier.mena@cloudx.com>
2025-04-22 21:40:25 -0500

    fix release

--- functions/index.py
+++ functions/index.py
@@ -6,7 +6,7 @@ from app import create_app
 app = create_app()
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
-def handler(request):
+async def fetch(request):
     """Handle the incoming request."""
     # Convert Cloudflare request to WSGI environment
     environ = {
